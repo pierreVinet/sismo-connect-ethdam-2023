90b875b654fcee40e1a41d10534d020dc345a053
Pierre <pierre.vinet17@gmail.com>
2023-05-21 10:55:54 +0200

    ward and angels pages

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,9 +1,11 @@
-import { exec } from "child_process";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
+
+  const buttons_home =
+    "w-72 h-20 border rounded-lg flex flex-row items-center justify-center hover:bg-white/10 duration-150";
 
   return (
     <div className="container">
@@ -12,16 +14,23 @@ export default function Home() {
         src="/assets/sismo-landing-art.svg"
         alt="sismo art"
       />
-      <h1 className="">Sismo Connect Onchain</h1>
+      <h1 className="">Secret Angels</h1>
       <section>
         <ul>
-          <li onClick={() => router.push("/secret-angels")}>
-            <h3>Secret Angels</h3>
+          <button onClick={() => router.push("/angels")} className={buttons_home}>
+            <h3 className="">Angels</h3>
             <p></p>
             {/* replace with the title below */}
             {/* <h3>Claim an airdrop anonymously</h3>
             <p>Sign an address with Sismo Connect where you wish to receive the airdrop</p> */}
-          </li>
+          </button>
+          <button onClick={() => router.push("/ward")} className={buttons_home}>
+            <h3>Ward</h3>
+            <p></p>
+            {/* replace with the title below */}
+            {/* <h3>Claim an airdrop anonymously</h3>
+            <p>Sign an address with Sismo Connect where you wish to receive the airdrop</p> */}
+          </button>
         </ul>
       </section>
     </div>
